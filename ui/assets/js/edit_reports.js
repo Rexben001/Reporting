6afedfad894af7eea5494f25e430795552ead2bd
Rexben <rexben.rb@gmail.com>
2018-12-05 00:42:59 +0100

    [Feature #162367227] Create a popup form

--- ui/assets/js/edit_reports.js
+++ ui/assets/js/edit_reports.js
@@ -1,12 +1,17 @@
 
 document.getElementById('edit').addEventListener('click', () => {
     const popup = document.getElementById('popup');
-    // let popStyle = popup.style.display;
+    const overlay = document.getElementById('overlay-pop');
 
     if(popup.style.display == 'block'){
         popup.style.display = 'none';
+        overlay.blur();
+
     }else{
         popup.style.display = 'block';
+
     }
     console.log('working')
+    overlay.blur();
+    console.log(overlay.blur())
 })
