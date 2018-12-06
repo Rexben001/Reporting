80354312d7bf52c854310da9470a7eb30e0361ae
Rexben <rexben.rb@gmail.com>
2018-12-06 12:11:02 +0100

    [Feature #162367216] Change the positioning of the popup form for Admin

--- ui/assets/js/edit_reports.js
+++ ui/assets/js/edit_reports.js
@@ -1,12 +1,14 @@
 
 document.getElementById('edit').addEventListener('click', () => {
     const popup = document.getElementById('popup');
+    const popupss = document.getElementById('popAdmin');
     const overlay = document.getElementById('overlay-pop');
     const locs = document.getElementById('loc');
     const stat = document.getElementById('status');
     const location = document.getElementById('location');
     const statuses = document.getElementById('statuses');
 
+    // console.log(popupss);
 
     const loc = locs.innerText;
     const status = stat.innerText;
@@ -16,24 +18,27 @@ document.getElementById('edit').addEventListener('click', () => {
     // let statusValue = sta[0];
 
 
-    if (popup.style.display == 'block') {
+    if (popup.style.display == 'block' || popup.style.display == 'block') {
         popup.style.display = 'none';
+        // popups.style.display = 'none';
         overlay.className = 'overlay-pops';
 
 
     } else {
         popup.style.display = 'block';
+        // popups.style.display = 'block';
         overlay.className = 'overlay-popup';
         location.value = loc;
         statuses.value = status;
 
 
     }
-
+console.log(popup, popups)
 });
 
 document.getElementById('save').addEventListener('click', () => {
     const popup = document.getElementById('popup');
+    const popups = document.getElementById('popAdmin');
     const overlay = document.getElementById('overlay-pop');
     const locs = document.getElementById('loc');
     const stat = document.getElementById('status');
@@ -43,6 +48,7 @@ document.getElementById('save').addEventListener('click', () => {
 
 
     popup.style.display = 'none';
+    // popups.style.display = 'none';
     overlay.className = 'overlay-pops';
     locs.innerHTML = location.value;
     stat.innerHTML = statuses.value;
