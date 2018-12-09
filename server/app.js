1cc812452da4fb0385c491982acf30a78f57f51c
Rexben <rexben.rb@gmail.com>
2018-12-09 10:15:37 +0100

    [Feature #162493588] Create a user

--- server/app.js
+++ server/app.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import users from './models/db';
 
 const app = express();
 const router = express.Router();
 
 app.use(bodyParser.urlencoded({
-  extended: true,
+  extended: false,
 }));
 app.use(bodyParser.json());
 
-router.get('/ben', (req, res) => {
-  res.json('Hello World!'); 
+router.get('/users', (req, res) => {
+  return res.json({
+    users
+  });
+
 });
 
-app.use('/api', router);
+app.use('/api/v1', router);
 
 app.listen(3000, () => {
   console.log('Listening to PORT 3000');
