cf4cfce07a74fa36010c08daca4a68ffb9f0fe5e
Rexben <30627428+Rexben001@users.noreply.github.com>
2019-01-17 21:35:49 +0100

    Update reportdb.js

--- server/models/reportdb.js
+++ server/models/reportdb.js
@@ -6,11 +6,12 @@ import dotenv from 'dotenv';
 // const { Pool } = pg;
 dotenv.config();
 const pool = new pg.Pool({
-  user: 'rex',
-  host: 'localhost',
-  database: 'report_db',
-  password: process.env.password,
-  port: 5432
+//   user: 'rex',
+//   host: 'localhost',
+//   database: 'report_db',
+//   password: process.env.password,
+//   port: 5432
+ connectionString: 'postgres://qmteovsj:Z8uaA3TyhniBQZDd6lBtZAQXRveoEfzt@baasu.db.elephantsql.com:5432/qmteovsj'
 });
 
 pool.on('connect', () => {
